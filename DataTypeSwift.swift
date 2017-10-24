13a9ecd6de43b7219bc256d66dee3b46cf295403
Juan Cantor <juan.cantor@utadeo.edu.co>
2017-10-23 21:28:32 -0500

    Cambio a archivo .swift

--- DataTypeSwift.swift
+++ DataTypeSwift.swift
@@ -2,3 +2,10 @@
 let dedosEnLaMano = 5
 let autosEnLaCochera = 2
 let estrellasEnElCielo = 1_000_000 // puedes usar _ entre numeros para mejorar la legibilidad
+
+let decimal = 17
+let binario = 0b10001 //17 en notacion binaria
+let octal = 0o21 //17 en notacion octal
+let hexadecimal = 0x11 //17 en notacion hexadecimal0b10001
+
+let suma = decimal + octal // #=> 34
